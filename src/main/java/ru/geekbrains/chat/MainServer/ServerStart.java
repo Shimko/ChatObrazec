6c7fa64d2ff944588cf80dd3c4272c1a6d7410fc
AlexeyShimko <shimko390@mail.ru>
2022-05-24 00:56:30 +0300

    очистил все чтоб сделать пулреквест

--- src/main/java/ru/geekbrains/chat/MainServer/ServerStart.java
+++ src/main/java/ru/geekbrains/chat/MainServer/ServerStart.java
@@ -1,8 +1,1 @@
 package ru.geekbrains.chat.MainServer;
-
-public class ServerStart {
-    public static void main(String[] args) {
-        ServerMain mainServer = new ServerMain();
-        mainServer.start();
-    }
-}
