9aec97690574ff535a32df30fc88ee04502e4681
AlexeyShimko <shimko390@mail.ru>
2022-05-24 00:21:15 +0300

    немного переделал помник и инфо

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,9 @@ module ru.geekbrains.chat {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.xerial.sqlitejdbc;
+    requires sqlite.jdbc;
     requires org.apache.commons.io;
 
-    requires org.kordamp.bootstrapfx.core;
 
     exports ru.geekbrains.chat.client;
     opens ru.geekbrains.chat.client to javafx.fxml;
